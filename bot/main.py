4292b507367325c8fae9ec4e415467d51f6107f5
H34V3N <948242589@qq.com>
2026-03-01 15:47:52 +0800

    add image saving model

--- bot/main.py
+++ bot/main.py
@@ -2,19 +2,24 @@ from ncatbot.core import BotClient
 from ncatbot.core import GroupMessage,PrivateMessage
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+from ncatbot.core.event.message_segment import Text, Image, File
+import os
 # 启动ai
 llm = ChatOpenAI(
-    base_url="", #自行设置模型api，
-    api_key="",
-    model=""
+    base_url="https://newapi.ashesb.com/v1", #自行设置模型api
+    api_key="sk-WYcyBGmplbbETXKvEtTjy3rIgrpNvpAcasoTOCLH7CktIZRq",
+    model="gemini-2.5-flash"
 )
 prompt = ChatPromptTemplate.from_messages([
 ("system", "你是一个助手，说话即可"),
 ("user", "{input}")])
 chain = prompt | llm
 masterQQ = str(input("请输入主人的qq号，在未来，只有收到这个qq号发来的消息才会有回复"))
-#编写ai对话模式使用的函数，初步计划输入想说的内容，返回回复
 
+#初始化一些需要的保存文件的路径
+fileadd = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'filesaved')
+iamgeadd = os.path.join(fileadd,'image')
+os.makedirs(iamgeadd,exist_ok=True)
 
 #目前会有相应的指令集
 commands = ['/aimode',
@@ -26,10 +31,23 @@ aiMode = False
 bot = BotClient()
 @bot.private_event()
 async def on_private_message(msg: PrivateMessage):
+
     global aiMode
     global commands
-    msg_text = msg.raw_message
+    fileArray = []
+    messageArray = msg.message
+    msg_text = ''
     user_id = msg.user_id
+
+    #检查是否有文件
+    for i in messageArray:
+        print(type(i))
+        if isinstance(i,(Image)):
+            fileArray.append(i)
+        if isinstance(i, Text):
+            msg_text += i.text + " "
+
+    print(messageArray)
     #对指令进行特殊处理
     if user_id == masterQQ and msg_text in commands:
         if msg_text == "/help":
@@ -59,11 +77,20 @@ async def on_private_message(msg: PrivateMessage):
 
     else:    
         #判断是不是我本人的qq号,和具体发送内容是否为指令
-        if user_id == masterQQ and msg_text != '/aimode':
+        if user_id == masterQQ:
+            if msg_text != '' and msg_text != '/aimode':
             #回复固定的私聊消息
-            await bot.api.post_private_msg(user_id, text="#ccb")
+                await bot.api.post_private_msg(user_id, text="#ccb")
+
+            #接收我本人发来的文件，并将其保存到同目录下的filesaved
+            if fileArray:
+                for i in fileArray:
+                    if isinstance(i,(Image)):
+                        await i.download(iamgeadd)
+                    else:
+                        await i.download(fileadd)
+                await bot.api.post_private_msg(user_id, text="文件已经收到，保存成功")
 
 
 if __name__ == '__main__':
-
     bot.run()
